2ceedb0929c88d5a6b8a7ddc3dcfaafb2349a64f
Pascal Thomet <pthomet@gmail.com>
2024-09-27 22:41:38 +0200

    Fail with glfw classic (i.e. included in emscripten): cannot resolve symbol glfwSetErrorCallback

--- repo_daft2/src/daft2/hello.cpp
+++ repo_daft2/src/daft2/hello.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+//! Display error message in the Console
+void consoleErrorHandler(int iErrorCode, char const *iErrorMessage)
+{
+  printf("glfwError: %d | %s\n", iErrorCode, iErrorMessage);
+}
 
 void hello()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    // set a callback for errors otherwise if there is a problem, we won't know
+    glfwSetErrorCallback(consoleErrorHandler);
+
+    // print the version on the console
+    printf("%s\n", glfwGetVersionString());
 }
